a6c6458c24106b4efd21af76c55721008b8127f4
kaplocs <haym2054@gmail.com>
2023-08-31 00:05:01 -0400

    got negated and display working

--- app.js
+++ app.js
@@ -1,101 +1,177 @@
+
 let operationArray = []
 let displayArray = [];
 let firstNumResult;
 let operator;
-let continueOperations = 1; 
-const clacDisplay = document.querySelector("#display-result");
-const button = document.getElementsByTagName('button');
-document.querySelectorAll("button").forEach(element => {
-        element.addEventListener('click', (event) => {
-                eventDecider(element);
-            });
-        });
-function eventDecider(element){
-    try{
-            if(element.classList[0] == "operator"){
-                let operator2 = element.value;
-                if(continueOperations==2){
-                    constNumber(operationArray);
-                    operationArray = [];
-                    operator = operator2;
-                    continueOperations=1;
-                }
-                if(continueOperations == 1){
-                    operator = element.value ;  
-                    continueOperations=2;
-                }
-            }
-            if(element.value == "clear" ){
-                operationArray = [];
-                continueOperations = 1;
-                console.log("cleared" )
-                console.log(`${operationArray}${continueOperations}`)
-            }
-            if(element.id =="mod"){
-                calcMod(operationArray);
-            }
-            if(element.id == "negate"){
-                NegateValue()
-                constNumber(operationArray);
-            }
-            if(element.value == "equals" ){
-                constNumber(operationArray);
-                operationArray = [];
-                //clacDisplay.textContent="";
-                //clacDisplay.textContent = firstNumResult;
-            }
-            if(element.value == "Backspace"){
-                operationArray.pop();
-                console.log(operationArray);
-            }
-            if(element.classList[0] == "operator"  || element.classList[0]== "operand"  || element.classList[0] == "decimal" ){
-                operationArray.push(element.value);
-                console.log(`${operationArray} ${continueOperations}`)
-            }
+let continueOperations = 1;
+let negated = false; 
+    function main(){
+        //let clacDisplay = document.querySelector("#display-result");
+        //const button = document.getElementsByTagName('button');
+        const result = document.createElement('span');
+        const decimal = document.getElementById(".");
+        const negateButton = document.getElementById("negate")
+        document.querySelectorAll("button").forEach(element => {
+                element.addEventListener('click', (event) => {
+                        eventDecider(element);
+                    });
+                });
+            
+        function eventDecider(element){
+            try{
+                    if(element.classList[0] == "operator"){
+                        decimal.disabled = false;
+                        negateButton.disabled = false;
+                        let operator2 = element.value;
+                        if(continueOperations==2){
+                            constNumber(operationArray);
+                            operationArray = [];
+                            operator = operator2;
+                            continueOperations=1;
+                        }
+                        if(continueOperations == 1){
+                            operator = element.value ;  
+                            continueOperations=2;
+                        }
+                    }
+                    if(element.value == "clear" ){
+                        operationArray = [];
+                        continueOperations = 1;
+                        negated = false;
+                        console.log("cleared")
+                        console.log(`${operationArray}${continueOperations}`)
+                        decimal.disabled = false;
+                        negateButton.disabled = false;
+                    }
+                    if(element.id =="mod"){
+                        calcMod(operationArray);
+                    }
+                    if(element.id == "negate"){
+                        if(negated){
+                            negated = false;
+                            operationArray.pop();
+                            console.log(operationArray);
+                        }else{
+                            negated = true;
+                            operationArray.push("@");
+                            console.log(operationArray);
+                        }
+                        console.log(operationArray);
+                        //negateButton.disabled = true;
+                    }
+                    if(element.value == "Backspace"){
+                        operationArray.pop();
+                        console.log(operationArray);
+                    }
+                    if(element.classList[0] == "operator"  || element.classList[0]== "operand"  || element.classList[0] == "decimal" ){
+                        operationArray.push(element.value);
+                        if(element.id == "."){
+                        decimal.disabled = true;
+                    }
+                        console.log(`${operationArray} ${continueOperations}`)
+                    }
+                    
                     if(element.classList[0] != "operand" && element.classList[0] != "decimal" ){
                         displayArray=[];
                     }else{
                         displayArray.push(element.value);
                     }
-                    clacDisplay.textContent = displayArray.join("");
-        }catch{}
+                    updateDisplay(displayArray.join(""));
+                    if(element.value == "equals" ){
+                        constNumber(operationArray);
+                        operationArray = [];
+                        negated = false;
+                        negateButton.disabled = false;
+                        decimal.disabled = false;
+                        updateDisplay(firstNumResult);
+                    }
+                }catch{}
+            }
+            document.addEventListener('keydown',(event)=>{
+                
+                if(event.key=="0"){
+                    eventDecider(document.getElementById("zero"));
+                }if(event.getModifierState("Control") && event.key =="5"){
+                    console.log("%");
+                    eventDecider(document.getElementById("zero"));
+                }else{
+                    console.log(event.key);
+                    eventDecider(document.getElementById(event.key));
+                }
+            })
     }
-    document.addEventListener('keydown',(event)=>{
-        
-        if(event.key=="0"){
-            eventDecider(document.getElementById("zero"));
-        }if(event.getModifierState("Control") && event.key =="5"){
-            console.log("%");
-            eventDecider(document.getElementById("zero"));
-        }else{
-
-            console.log(event.key);
-            eventDecider(document.getElementById(event.key));
-        }
-    })
-    function NegateValue(){
-             
+main();
+    function updateDisplay(element){
+        let clacDisplay = document.querySelector("#display-result");
+        clacDisplay.textContent = element;
     }
     function constNumber(Array){
-    let indexOfOperator = Array.indexOf(operator);
-    let firstNum;
-    let secondNum;
-    secondNum  = Number(Array.splice(indexOfOperator+1,Array.length).join(""));
-        if(indexOfOperator -1 == -1){
+        let indexOfOperator = Array.indexOf(operator);
+        
+        let firstNum;
+        let secondNum;
+        let splitFirst; 
+        let splitSecond;
+    
+        splitSecond = Array.splice(indexOfOperator+1,Array.length);
+        splitFirst =  Array.splice(0,indexOfOperator);
+        
+            secondNum = removeNegatedElement(splitSecond.indexOf("@"),splitSecond);
+        if(indexOfOperator -1 === -1){
             firstNum = firstNumResult;
-            calculate(firstNumResult,operator,secondNum);
-            
+            calculate(firstNumResult,operator,secondNum);    
         }else{
-            firstNum = Number(Array.splice(0,indexOfOperator).join(""));
-            
+            firstNum = removeNegatedElement(splitFirst.indexOf("@"),splitFirst);
         }
-    calculate(firstNum,operator,secondNum);
-    continueOperations=1;
-    console.log(firstNum);
-    console.log(operator);
-    console.log(secondNum);
-    console.log(firstNumResult);
+        console.log(`${splitFirst} splits ${splitSecond} .`)
+        
+    
+        console.log("index Of"+ splitFirst.indexOf("@"));
+        firstNumResult = calculate(firstNum,operator,secondNum);
+        continueOperations = 1;
+        console.log(firstNum);
+        console.log(operator);
+        console.log(secondNum);
+        console.log(firstNumResult);
     }
+    function removeNegatedElement(index , NumPosition ){
+        if(index === -1){
+            console.log(Number(NumPosition.join("")));
+            return Number(NumPosition.join(""));
+        } 
+        if (index >= 0 && index < NumPosition.length) {
+            NumPosition.splice(index, 1);
+        }
+        return -Number(NumPosition.join(""));
+    }
+    
+            /*if(indexOfnegate>indexOfOperator){
+                secondNum*=-1
+            }else if (indexOfnegate<indexOfOperator){
+                firstNum*=-1 
+            }else{
+                firstNum*=-1;
+                secondNum*=-1;
+            } 
+        }else{
+    
+            secondNum  = Number(Array.splice(indexOfOperator+1,Array.length).join(""));
+        
+        }
+    function splitArray(Array){
+        
+        
+
+        splitFirst =  Array.splice(0,indexOfOperator).join("");
+        splitSecond = Array.splice(indexOfOperator+1,Array.length).join("");
+        firstNum = removeNegatedElement(Array.indexOf("\""),splitFirst);
+        secondNum = removeNegatedElement(Array.indexOf("\""),splitSecond);
+    }*/
+   
+        
+        
+    
+    
     function calcMod(modNum){
         let firstNum = Number(modNum.join(""));
         firstNumResult = firstNum / 100;
@@ -105,17 +181,18 @@ function eventDecider(element){
     function calculate(firstNum,operator,secondNum){
         switch(operator){
             case "+":
-                firstNumResult = firstNum + secondNum;
-                break;
+                return  firstNum+= secondNum;
+                
             case "-":
-                firstNumResult = firstNum - secondNum;
-                break;
+                return  firstNum-= secondNum;
+                
             case '/':
-                firstNumResult = firstNum / secondNum; 
-                break;
+                return  firstNum/= secondNum; 
+                
             case "*":
-                firstNumResult = firstNum * secondNum;
-                break;
+                return  firstNum*= secondNum;
+                
         }
         
     }
+   
